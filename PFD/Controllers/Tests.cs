4896f6de37831a5b1e22d9532f827704b959abbd
Koay Yeow Teik <115933415+KoayYeowTeik@users.noreply.github.com>
2024-01-14 13:35:51 +0800

    force push remove corruption

--- PFD/Controllers/Tests.cs
+++ PFD/Controllers/Tests.cs
@@ -14,21 +14,13 @@ namespace PFD.Controllers
 		}
 		public IActionResult EmotionDetection()
 		{
-			//get video of camera
-			// load the models
-			// run the faceapi detection code to get the emotions (check the detections variable for processing)
 			return View();
 		}
 		public IActionResult FaceUpload()
 		{
-			// get video of camera
-			// load the facapi model to find face
-			// take screenshot of face when face found
-			// check if detection is >0.8 (do when integrate)
-			// convert image to blob and upload to firebase storage
 			return View();
 		}
-		public void FaceRecognition()
+		public IActionResult FaceRecognition()
         {
 			return View();
 			/*try
@@ -50,9 +42,7 @@ namespace PFD.Controllers
                     sys.path.append(@"..\python_dependencies");
                     Console.WriteLine(test.ReturnString("Go to logout"));
 
-				}
-
-
+				}*/
         }
         public IActionResult Langchain()
 		{
@@ -61,9 +51,6 @@ namespace PFD.Controllers
 		}
 		public IActionResult WhisperAI()
 		{
-			// on the microphone and record voices => seperate the existing voice into blobs
-			// pass the blob sound files into whisper to pass to langchain in javascript
-			// get the transcript as a string
 			return View();
 		}
 	}
--- PFD/Program.cs
+++ PFD/Program.cs
@@ -36,6 +36,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Tests}/{action=WhisperAI}/{id?}");
+    pattern: "{controller=Tests}/{action=FaceRecognition}/{id?}");
 
 app.Run();
