89e16255611b20db24f4d3bfac7fcafd2e28c37f
limjunhui <110986279+ljunhui05@users.noreply.github.com>
2024-01-17 16:28:58 +0800

    Changed system.text.json to newtonsoft.json

--- PFD/Controllers/UserController.cs
+++ PFD/Controllers/UserController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PFD_ASG.Models;
 using PFD_ASG.DAL;
-using System.Text.Json;
+using Newtonsoft.Json;
 using MongoDB.Driver;
 using static MongoDB.Driver.WriteConcern;
 using System.Threading;
